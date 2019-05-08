1c19688dee7b4aa74feb51d00b7c28894ba1dfff
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-05-07 23:23:58 -0400

    home done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CameraService } from 'src/app/services/camera.service';
+import { ImageRecognitionService } from 'src/app/services/image-recognition.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: [ './home.component.sass' ]
 })
 export class HomeComponent implements OnInit {
+	$cameraData: Observable<any[]>;
+	$recognitionData: Observable<any>;
+	constructor(private cameraService: CameraService, private recognitionService: ImageRecognitionService) {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	OnGetCameraData(): void {
+		this.$cameraData = this.cameraService.getCameraData();
+	}
 
+	OnGetImageData(): void {
+		this.$recognitionData = this.recognitionService.getImageData();
+	}
 }
