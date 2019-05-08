02107a777d50afb461e7f5096b3c5970bb2c8003
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-05-07 23:25:27 -0400

    rekonition service

--- src/app/services/image-recognition.service.ts
+++ src/app/services/image-recognition.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ImageRecognitionService {
+	BASE_URL = 'https://2pfwj7aw0a.execute-api.us-west-2.amazonaws.com/hackathon/rekognition';
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
+	getImageData() {
+		return this.http.get(this.BASE_URL, {
+			params: {
+				key: 'test.jpg'
+			}
+		});
+	}
 }
