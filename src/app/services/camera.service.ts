93380c08c40125c264bcd47087e865c366ba717c
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-05-07 23:23:47 -0400

    camera service done

--- src/app/services/camera.service.ts
+++ src/app/services/camera.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class CameraService {
+	BASE_URL = 'https://2pfwj7aw0a.execute-api.us-west-2.amazonaws.com/hackathon/hackaton-data';
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
+	getCameraData(): Observable<any> {
+		return this.http.get<Observable<any>>(this.BASE_URL);
+	}
 }
